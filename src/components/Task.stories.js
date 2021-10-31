0912ceb8a093058cc5d85034ad69b4d4e66bce5a
Seunghyun Yoo <seunghyunyoo@Seunghyunui-MacBookPro.local>
2021-10-31 20:14:50 +0900

    feat: 스토리북 공식문서 예제 작업중

---
+++ src/components/Task.stories.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Task from "./Task";
+
+export default {
+  component: Task,
+  title: "Task",
+};
+
+const Template = (args) => <Task {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  task: {
+    id: "1",
+    title: "Test Default",
+    state: "Task_INBOX",
+    updatedAt: new Date(2018, 0, 1, 9, 0),
+  },
+};
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+  task: {
+    ...Default.args.task,
+    // title: "Test Pinned",
+    state: "TASK_PINNED",
+  },
+};
+
+export const Archived = Template.bind({});
+Archived.args = {
+  task: {
+    ...Default.args.task,
+    // title: "Test Archived",
+    state: "TASK_ARCHIVED",
+  },
+};
